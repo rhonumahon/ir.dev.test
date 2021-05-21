ba45fce9ab837f1a8f0c66f811176e529416f9cb
IR <na@intelligentreach.com>
2021-05-21 13:35:48 +0800

    FEAT: Import CommonModule to configure angular directives

--- src/app/products/products.module.ts
+++ src/app/products/products.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
-
+import { CommonModule } from "@angular/common";
 import { ProductsGridComponent } from "./products-grid.component";
 
 @NgModule({
   imports: [
+    CommonModule
   ],
   declarations: [
     ProductsGridComponent

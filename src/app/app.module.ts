f6a0e996383fb3313b8334f8ddce2b365b5a957f
IR <na@intelligentreach.com>
2021-05-21 20:43:05 +0800

    FEAT: Import FormsModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
@@ -10,7 +11,8 @@ import { ProductsModule } from "./products/products.module";
   ],
   imports: [
     BrowserModule,
-    ProductsModule
+    ProductsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]

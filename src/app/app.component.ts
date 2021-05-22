6b644fa218908760ceae4357dc268fb343cd4330
IR <na@intelligentreach.com>
2021-05-22 08:17:22 +0800

    FEAT: Add logic to get dropdown options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
 import { Component} from "@angular/core";
+import { ProductsService } from "./products/products.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
+  providers: [ProductsService]
 })
 export class AppComponent {
   title = "ir-dev-test";
   searchInput: string;
+  dropdown: any;
+
+  constructor(private prodService: ProductsService){
+    this.prodService.getProducts().subscribe(event => {
+      const values = event.map(item => item.brand);
+      this.dropdown = [...new Set(values)];
+      
+    })
+  }
 }
